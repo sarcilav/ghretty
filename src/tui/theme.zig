3b0261f63716d1705cdd2f8384977e5c4b6eb554
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2026-02-26 11:13:08 +0100

    Migrate colors to vaxis 0.5

--- src/tui/theme.zig
+++ src/tui/theme.zig
@@ -1,28 +1,32 @@
 const vaxis = @import("vaxis");
 
+// ANSI color indices:
+// 0: black, 1: red, 2: green, 3: yellow, 4: blue, 5: magenta, 6: cyan, 7: white
+// 8-15: bright versions
+
 pub const selected_row_style = vaxis.Style{
-    .fg = vaxis.Color.ansi(.black),
-    .bg = vaxis.Color.ansi(.white),
+    .fg = .{ .index = 0 },   // Black
+    .bg = .{ .index = 15 },  // Bright white
     .bold = true,
 };
 
 pub const normal_style = vaxis.Style{
-    .fg = vaxis.Color.ansi(.white),
-    .bg = vaxis.Color.ansi(.black),
+    .fg = .{ .index = 7 },   // White
+    .bg = .{ .index = 0 },   // Black
 };
 
 pub const header_style = vaxis.Style{
-    .fg = vaxis.Color.ansi(.cyan),
+    .fg = .{ .index = 6 },   // Cyan
     .bold = true,
     .underline = true,
 };
 
 pub const error_style = vaxis.Style{
-    .fg = vaxis.Color.ansi(.red),
+    .fg = .{ .index = 1 },   // Red
     .bold = true,
 };
 
 pub const loading_style = vaxis.Style{
-    .fg = vaxis.Color.ansi(.yellow),
+    .fg = .{ .index = 3 },   // Yellow
     .bold = true,
 };
